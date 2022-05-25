42411ca8b0173972615026b85fde8d341d68cb2f
Mihary <raharitsiresy@gmail.com>
2022-05-25 14:22:18 +0300

    - Intégration de la homepage - Ajout react-smooth-scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import logo from './logo.svg';
-// import './App.css';
-import './custom.scss';
-import Approutes from './AppRoutes';
+import './App.css';
+import "./custom.scss";
+import Approutes from "./AppRoutes";
+import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
+import Scroll from "./components/SmoothScroll/SmoothScroll";
 
 const TopNav = () => {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -10,11 +12,11 @@ const TopNav = () => {
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary fixed-top rounded">
-      <div className="container">
-        <a className="navbar-brand text-white font-weight-bolder" href="/">
+    <nav className="navbar navbar-expand-lg bg-primary fixed-top" id="top-nav">
+      <div className="container-fluid" style={{paddingLeft: "80px"}}>
+        <a className="navbar-brand text-white font-weight-bolder mt-2" href="/">
           {/* <img src="https://getbootstrap.com/docs/4.6/assets/brand/bootstrap-solid.svg" alt="Logo" width="36" height="36" className="vertical-align-middle" /> */}
-          <h4 className="">Movies&Me</h4>
+          <h4 className="">Food.</h4>
         </a>
         <button
           className="custom-toggler navbar-toggler"
@@ -30,21 +32,19 @@ const TopNav = () => {
         </button>
 
         <div
-          className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
-          id="navbarsExample09"
+          // className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+          // id="navbarsExample09"
         >
           <div className="d-flex justify-content-center justify-content-md-end w-100">
-            <a className="nav-link text-white" href="/contact">
-              News
-            </a>
+            
             <a className="nav-link text-white" href="/login">
-              Favorites
+              +261 34 12 000 00
             </a>
             <a
               href="/request-demo"
-              className="btn btn-sm btn-dark nav-link text-white float-right"
+              className="btn btn-sm btn-outline-secondary nav-link text-white float-right"
             >
-              Sign in
+              Réservations
             </a>
           </div>
         </div>
@@ -55,25 +55,19 @@ const TopNav = () => {
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <button className='btn btn-primary'>Hello</button>
-    //   </header>
-    //   <div className="container">
-    //     <Approutes />
-    //   </div>
-    // </div>
-    <div className="App">
-      <TopNav />
-      <div className="auth-wrapper pt-5">
-        {/* <Menu />  */}
-        <div className="container pt-5">
-          {/* The project routes  */}
-          <Approutes />
-          {/* <NumeroField /> */}
+    <>
+      <BurgerMenu />
+      <div className="App">
+        <TopNav />
+        <Scroll />
+        <div className="auth-wrapper">
+          <div className="container" style={{paddingTop: "80px"}}>
+            {/* The project routes  */}
+            <Approutes />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,66 @@
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="container">
+      <div className="row text-white py-5">
+        <div
+          className="col-12 col-md-12"
+          style={{
+            background:
+              "url('https://picsum.photos/id/1003/400/600') no-repeat bottom right",
+            minHeight: "500px",
+          }}
+        >
+          <div className="row">
+            <div className="col-md-6">
+              <h1
+                className="font-rufina-bold float-right mb-4"
+                style={{ fontSize: "70px" }}
+              >
+                Healthy eating <br /> is important <br /> part of lifestyle
+              </h1>
+
+              <small>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                eveniet debitis adipisci esse laboriosam nisi a consectetur,
+                nulla nostrum eaque!
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        Hello world hello world Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Distinctio est odio nulla suscipit excepturi culpa
+        voluptatum soluta voluptatibus qui, porro expedita quisquam quam vero
+        harum doloremque inventore facilis nesciunt reprehenderit?
+      </div>
+      <div className="row text-white py-5">
+        <div className="col-12 col-md-8">
+          <img
+            src="https://picsum.photos/id/1003/500/300"
+            alt="Start to plan your diet"
+            className="mb-5"
+          />
+          <h2>Start to plan your diet today</h2>
+          <p style={{maxWidth: "400px"}} className="small">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Modi, alias? Dolore, praesentium.
+          </p>
+        </div>
+        <div className="col-md-4">
+          <p className="small mb-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            architecto quasi soluta provident odio delectus adipisci.
+          </p>
+          <img
+            src="https://picsum.photos/id/1003/300/450"
+            alt="Start to plan your diet"
+            className="mb-3"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
